8e19fe636f7d16f688abad125369316630682b05
Galina Eccles <geccles01@gmail.com>
2020-03-13 22:42:11 -0400

    Update notifications.js

--- routes/notifications.js
+++ routes/notifications.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 var axios = require('axios')
 
-var slack_url = 'https://hooks.slack.com/services/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';
+var slack_url = 'https://hooks.slack.com/${add a channel here}';
 var payload_slack = {username: 'APPrise Mobile Slack Notification', text: 'Slack Notification Test Axios'};
 /* GET slack notification listing. */
 router.get('/', function(req, res, next) {
